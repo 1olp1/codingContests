efdcc9479be8a15bd19ba431e068cdd6edee4aff
Oliver <oliver.b.haeussler@gmail.com>
2023-09-17 04:20:50 +0200

    deleted not needed batch file

--- makeRepo.bat
+++
@@ -1,7 +0,0 @@
-echo "# codingContests" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:1olp1/codingContests.git
-git push -u origin main
